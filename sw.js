f68dfc6651abe2ecc4e56851325a673f63ff8abd
DarMaroc <dar.maroc4@gmail.com>
2026-09-23 15:17:11 +0000

    admin: visual settings with upload buttons (logo/banner/background)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* DarMaroc - Service Worker (PWA) v1.7.0 */
-const VERSION = 'darmaroc-v1.7.1';
-const CORE_CACHE = 'darmaroc-core-v17b';
+/* DarMaroc - Service Worker (PWA) v1.7.2 */
+const VERSION = 'darmaroc-v1.7.2';
+const CORE_CACHE = 'darmaroc-core-v18';
 const IMAGE_CACHE = 'darmaroc-images-v2';
 const API_CACHE = 'darmaroc-api-v1';
 
